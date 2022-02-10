e98a29a8b77bc94c370bc9fef942c4d57860493f
Anna Tolokonnikova <zzz.moi.zzz.moi@gmail.com>
2022-02-10 14:23:01 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
-  // ваш код...
+  let tbodyRows = Array.from(table.lastElementChild.rows);
+  let tableNew = tbodyRows.map((item) => {
+    let cells = item.cells;
+
+    cells[3].dataset.available == 'true' ? item.classList.add('available')
+          : item.classList.add('unavailable');
+
+    if (!cells[3].dataset.available){
+      item.setAttribute('hidden', true);
+    }
+
+    cells[2].textContent == 'm' ? item.classList.add('male') : item.classList.add('female');
+
+    if(parseInt(cells[1].textContent) < 18){
+      item.style.textDecoration = 'line-through';
+    }
+
+  });
+  return tableNew;
 }
